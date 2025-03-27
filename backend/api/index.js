b0339045d72b4f79fca1fea22c708697a6fd36f9
NishantKhorwal <khorwalnishant@gmail.com>
2025-03-27 15:13:48 +0530

    a

--- backend/api/index.js
+++ backend/api/index.js
@@ -57,8 +57,8 @@ app.use(cors({
 }));
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '50mb' }));  // Adjust the size as needed
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use('/api/project', projectRoutes);
 app.use('/api/user', userRoutes);
