a0f9a2e5bcb78073a0b3965bf0917830976f908f
NishantKhorwal <khorwalnishant@gmail.com>
2025-03-27 14:55:40 +0530

    fixed cors error

--- backend/api/index.js
+++ backend/api/index.js
@@ -50,10 +50,19 @@ const io = new Server(server, {
 
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200,
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
