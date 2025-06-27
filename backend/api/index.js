4a4b014b54f1da5a984e5a901a58bcac8885d0e8
NishantKhorwal <khorwalnishant@gmail.com>
2025-06-27 12:57:58 +0530

    a

--- backend/api/index.js
+++ backend/api/index.js
@@ -33,7 +33,7 @@ const server = createServer(app);
 // CORS Configuration (Must be on Top)
 const allowedOrigins = [
   "https://rofconnect.com",
-  "https://landingpage.rofpravasa.com/",
+  "https://landingpage.rofpravasa.com",
   "https://rofinventorymanagement.netlify.app",
 ];
 
